1f65a763740a85eaf0dc769a0ff26cfc99e6375b
kujbika <43244473+kujbika@users.noreply.github.com>
2019-01-23 00:09:13 +0100

    Update

--- momentum_factor.R
+++ momentum_factor.R
@@ -7,13 +7,8 @@ source("C:/Users/User/Documents/GitHub/Anomaly-trading-in-FX-market/data_handle.
 #over the previous f = 1,3,6,9,12 months. These portfolios are held for
 #h = 1,3,6,9,12 months. 1/3, 1/3, 1/3 weights will be assigned to the best
 #3 crcies, whereas -1/3, -1/3, -1/3 will be assigned to the "losers".(dollar neutral)
-FxReturn <- function(intrate, price1, price0){
-  #be careful on the rates being annualized etc
-  return (log( 1 + intrate )+
-            log( ( 1 + price1 ) / ( 1 + price0 )) )
-}
 
-#tseries <- DataFilterer(1, 'Spot')
+
 Mom <- function(f, tseries){
   #f denotes months, and I calculate w working days
   return (na.omit(tseries) %>%
@@ -21,6 +16,3 @@ Mom <- function(f, tseries){
                             diff( Spot, lag = f * 22 ) ) ) )
 }
 
-PortfolioSort <- function(vector){
-  return ( transpose( data.table( vector ) ) %>% sort( ) )
-}
--- momentum_trading.R
+++ momentum_trading.R
@@ -2,7 +2,17 @@ source("C:/Users/User/Documents/GitHub/Anomaly-trading-in-FX-market/momentum_fac
 TableMaker <- function(f = 1, freq = 252){
   workingtable <- Mom( f, SpotInterest( 1, freq ) )
   for (i in 2:9) workingtable = dplyr::left_join( workingtable, Mom( f, SpotInterest( i, freq ) ), by = 'Date')
+  #log(spot) price for all currency
+  workingtable[, seq(2, 28, 3)] = workingtable[, seq(2, 28, 3)] %>% 
+                                  mutate_all( funs( replace( ., is.na( . ), 1 ) ) )%>% 
+                                  log( )
+  #log(1+return) for all currency
+  workingtable[, seq(3, 28, 3)] = (1 + workingtable[, seq(3, 28, 3)] ) %>% 
+                                  mutate_all( funs( replace( ., is.na( . ), 1 ) ) )  %>% 
+                                  log( )
+  
   moms <- workingtable[ -(1 : (f * 22)), c( 1, seq( 4, 28, 3 ) ) ]
+  
   return ( list( workingtable[ -(1 : (f * 22)), ], moms ) )
 }
 WeightAssigner <- function(rowfromtable){
@@ -13,10 +23,46 @@ WeightAssigner <- function(rowfromtable){
   return (transpose(data.table(weight)))
 }
 
-Trade <- function(workingtable){
-  weights <- WeightAssigner( workingtable[ 1, ])
-  for (i in 2:nrow(workingtable)) weights = weights %>%
-      rbind( WeightAssigner( workingtable[ i, ] ) )
-  return (weights)
+Trade <- function(f = 1, h = 1){
+  #h is in days, f is in months!!!!
+  freq <- round(252 / h)
+  workingtable <- TableMaker(f, freq)
+  weights <- WeightAssigner( workingtable[[ 2 ]][ 1, ]) %>%
+             slice( rep( row_number( ), h ) ) 
+  realloc <- seq(1 + h, nrow(workingtable[[ 2 ]]), h)
+  for (i in realloc){
+    newweight <- WeightAssigner( workingtable[[ 2 ]][ i, ] ) %>%
+                 slice( rep( row_number( ), h ) ) 
+    weights = weights %>% rbind(newweight)
+  }
+  weights = workingtable[[2]][ , 1] %>% cbind(weights)
+  return ( list( workingtable[[ 1 ]], weights ) )
 }
-trading <- Trade(TableMaker()[[2]])
+
+StrategyEvaluation <- function(f = 1, h = 1){
+  #this is the actual evaluation of the MOM based strategy
+  #f is the look-back time in months (for calculating Momentum),
+  #whereas h is the portfolio reallocation frequency in days(holding period)
+  #in this part I assume no transaction costs.
+  trade <- Trade(f, h)
+  logdiff <- data.table(trade[[1]][,-1])[,lapply(.SD,diff)]
+  FxReturn <- function(rowidx){
+    #this function only works with the proper configuration of the table
+    spot_return <- logdiff[rowidx + 1,c(seq(1,27,3))]
+    intrate_return <- trade[[1]][rowidx, seq(3,27,3)]
+    
+    returns_each <- data.table(as.matrix(spot_return) + as.matrix(intrate_return))
+    colnames(returns_each) <- c('V1', 'V2', 'V3',
+                                'V4', 'V5', 'V6',
+                                'V7', 'V8', 'V9')
+    return ( returns_each )
+  }
+  returns_each <- data.table( 0, 0, 0, 0, 0, 0, 0, 0, 0 )
+  for (i in 2 : nrow( trade[[ 1 ]]) ){
+    returns_each = returns_each %>% rbind( FxReturn( i - 1 ) )
+  }
+  portfolio_return =data.table(dailyreturn = diag(as.matrix(trade[[2]][,-1]) %*% as.matrix(t(ret))))
+  portfolio_return = trade[[2]][,1] %>% cbind(portfolio_return)
+  perannum <- sum(portfolio_return$dailyreturn, na.rm = T)/nrow(portfolio_return)*252
+  return (list(portfolio_return, perannum))
+}
